5edbff437f8e0a3acc6399928d692e8127892161
TheScarastic <warabhishek@gmail.com>
2018-12-29 09:00:05 +0530

    [HAX]rosy: delete goodix data if other slot is taking up space

--- device.mk
+++ device.mk
@@ -300,7 +300,9 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.target.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    init.rosy rc \
+    init.goodix.sh
 
 PRODUCT_PACKAGES += \
     init.qcom.bt.sh \
